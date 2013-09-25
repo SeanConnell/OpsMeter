9b6bfdd55d1cee266828bdc11856af7effd513f7
Sean Connell <sean@seanconnell.net>
2013-09-24 18:35:26 -0700

    Added code to send data from POST to /STATE to MCU. Has some basic sanity checks in parsing input. Unverified currently.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ OpsMeter
 ========
 
 A small webserver and arduino program that receives information to display and then displays it on an RGB bar. 
+
+Install that udev rule to /etc/udev/rules.d/72-micro-devel.rules to ensure that the device always shows up as the 
+same comm port. Otherwise RESET commands will cause issues with dropped comm ports, as the server will have a harder time finding the comm port. Currently won't be able to anyways however.
--- opsmeter_server.go
+++ opsmeter_server.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"bytes"
+	"encoding/hex"
+	"errors"
+	"fmt"
 	"github.com/hoisie/web"
 	"github.com/tarm/goserial"
 	"io"
 	"log"
-	"fmt"
-	"encoding/hex"
+	"strconv"
 )
 
 /*
 Runs a small webserver that communicates with an embedded lighting controller.
 */
 
-//TODO: make this format a packet with data in the format COMMAND\r[DATA BYTES]
-func send(command string, serial io.ReadWriteCloser) {
-	serial.Write([]byte(command + "\r"))
+func send(data string, serial io.ReadWriteCloser) {
+	serial.Write([]byte(data + "\r"))
 }
 
-func recieve(size int, serial io.ReadWriteCloser) ([]byte , error){
+func recieve(size int, serial io.ReadWriteCloser) ([]byte, error) {
 	b := make([]byte, size)
 	n, err := serial.Read(b)
 	//don't handle reading fewer bytes than expected for now
@@ -46,7 +47,7 @@ func reset(ctx *web.Context, serial io.ReadWriteCloser) {
 func output_state(ctx *web.Context, serial io.ReadWriteCloser) {
 	send("GETSTATE", serial)
 	msg, err := recieve(96, serial)
-	if err != nil{
+	if err != nil {
 		ctx.WriteString(err.Error())
 	} else {
 		out := hex.EncodeToString(msg)
@@ -55,17 +56,61 @@ func output_state(ctx *web.Context, serial io.ReadWriteCloser) {
 }
 
 //Used to handle getting input data to set 
-//TODO implement
 func input_data(ctx *web.Context, serial io.ReadWriteCloser) {
-	retval := "SETOUPUTDATA: "
+	retval := "SETSTATE: "
+	output_state := make([]byte, 96)
 	for k, v := range ctx.Params {
 		retval += k + "->" + v + ","
+		color_loc, err := parse_color_location(k)
+		if err != nil {
+			ctx.WriteString(err.Error())
+			return
+		}
+		if len(v) != 1 {
+			ctx.WriteString("Value must be 1 byte long, eg between 0-255")
+		}
+		output_state[color_loc] = ([]byte(v))[0] //sketchy cast, fix with validation later
 	}
+	retval += string(output_state)
 	retval += "\n"
 	var buf bytes.Buffer
 	buf.WriteString(retval)
-	send("SETOUTPUTDATA", serial)
+	send("SETSTATE"+string(output_state), serial)
 	io.Copy(ctx, &buf)
+}
+
+func parse_color_location(color_index string) (uint64, error) {
+	if len(color_index) != 3 {
+		return 0, errors.New("Improper color index length of not 3")
+	}
+	color_pos, err := get_pos(color_index[len(color_index)-1:])
+	if err != nil {
+		return 0, err
+	}
+	//convert string to unsigned base 10 int from 8 bits
+	led_pos, err := strconv.ParseUint((color_index[:len(color_index)-1]), 10, 8)
+	if err != nil {
+		return 0, err
+	}
+	if led_pos >= 32 {
+		return 0, errors.New("The led strip is only 32 leds long, zero indexed")
+	}
+	//* 3 because each LED is three wide
+	return (led_pos * 3) + uint64(color_pos), nil
+}
+
+func get_pos(name string) (int, error) {
+	switch name {
+	case "r", "R":
+		return 0, nil
+	case "g", "G":
+		return 1, nil
+	case "b", "B":
+		return 2, nil
+	default:
+		return 0, errors.New("color position must end with r,g,b (case insensitive)")
+	}
+	return 0, errors.New("This should never happen")
 }
 
 //Abstract/encapsulate library for serial usage
@@ -82,11 +127,11 @@ func initialize_serial(name string, baud int) (io.ReadWriteCloser, error) {
 func commands_index(ctx *web.Context) string {
 	var commands = "Commands index for <b>OPS LIGHTS</b>" +
 		"<ul>" +
-		"<li>POST to /SETOUTPUT with 32 3-tuples of 8 bit [R,G,B] values in a format like:<br>" +
-		"    0R=138&0G=12&0B=241&1R=23& etc ...<br>" +
-		"    Any values not set will default to 0,0,0 eg black.</li>" +
-		"<li>POST to /RESET will send a reset command to the light controller and reset state.</li>" +
 		"<li>GET to /STATE will read device state and display it here.</li>" +
+		"<li>POST to /STATE with 32 3-tuples of 8 bit [R,G,B] values in a format like:<br>" +
+		"    0R=138&0G=12&0B=241&1R=23& etc ...<br>" +
+		"    The whole array must be supplied.</li>" +
+		"<li>POST to /RESET will send a reset command to the light controller and reset state.</li>" +
 		"<li>GET to /* (anything but /STATE) will display this command list.</li>" +
 		"</ul>" +
 		"<br>" +
@@ -97,7 +142,7 @@ func commands_index(ctx *web.Context) string {
 //Setup handlers for different addresses and HTTP methods here
 func main() {
 	//setup serial for use
-	s, err := initialize_serial("/dev/ttyACM0", 115200)
+	s, err := initialize_serial("/dev/ttyACM1", 115200)
 	if err != nil {
 		log.Fatal("Couldn't open comm port. ", err)
 	}
@@ -106,12 +151,12 @@ func main() {
 	//close over lower level function to prevent reinitialization of serial port
 	send_input_data := func(ctx *web.Context) { input_data(ctx, s) }
 	send_reset := func(ctx *web.Context) { reset(ctx, s) }
-	get_output_state := func(ctx *web.Context) {output_state(ctx, s) }
+	get_output_state := func(ctx *web.Context) { output_state(ctx, s) }
 
 	//setup handlers for various commands/addresses
-	web.Get("/STATE", get_output_state)         //Get state from device and return it
-	web.Get("/.*", commands_index)          //Get help
-	web.Put("/STATE", send_input_data) //Set the output of the device
-	web.Put("/RESET", send_reset)          //Reset device
+	web.Get("/STATE", get_output_state) //Get state from device and return it
+	web.Get("/.*", commands_index)      //Get help
+	web.Put("/STATE", send_input_data)  //Set the output of the device
+	web.Put("/RESET", send_reset)       //Reset device
 	web.Run("0.0.0.0:8089")
 }
